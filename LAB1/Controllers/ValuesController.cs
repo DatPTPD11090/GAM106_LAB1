dbbfb3cd0df4c9de6e904ca64cfd7f8b131ddf44
DatPTPD11090 <162497226+DatPTPD11090@users.noreply.github.com>
2024-11-09 14:35:57 +0700

    Lab 1

---
+++ LAB1/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using LAB1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Lab1Controller : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            GAM106LAB1 Game106 = new GAM106LAB1
+            {
+                CourseName = "Back-End Game Programming",
+                CourseCode = "GAM106",
+                Name = "Phan Tuan Dat",
+                StudentCode = "PD11090",
+                Class = "GA19301",
+            };
+            int status = 1;
+            string message = "Get data sucess!";
+            var data = new { status, message, Game106 };
+            return new JsonResult(data);
+        }
+    }
+}
